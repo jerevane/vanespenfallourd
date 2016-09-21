f336b561bf8d3160a08cd56b956acb61655358fc
Jeremie <jeremievanespen@hotmail.fr>
2016-09-21 12:07:47 +0200

    test prof

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 
-//du texte Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+//du textequelque chose Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
