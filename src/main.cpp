25bf776994198687f22f499ca6b87044534900d6
Jeremie <jeremievanespen@hotmail.fr>
2016-09-21 09:26:05 +0200

    modif by jeremie

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 
-// Un truc de ouf genre plus gros que ma planette Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+// Un truc de ouf genre plus gros que ma planetteuuuuuuuuuuuuuuuuuuuuu Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
