d54b1f5d1d2129ebe44ec3624ad1201fc4a697ac
Fallourd <yohann.fallourd@gmail.com>
2016-09-21 08:34:33 +0200

    Modif test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+// Un trucLes lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
