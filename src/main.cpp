ea4886e356dce62540f4db8655ef0aac29485e72
Jeremie <jeremievanespen@hotmail.fr>
2016-09-21 08:56:35 +0200

    modif by ide

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-C
-// Un truc de ouf genre plus gros que ma planet Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+
+// Un truc de ouf genre plus gros que ma planette Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
